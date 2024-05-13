38c0d5cc0a517e3910ac65e6694d17e257530695
Kelvin <kelvinmusipha@gmail.com>
2024-05-13 05:47:37 -0700

    llkoi

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  document.getElementById ('mySidenav')
+  .addEventListener ('click', function() {
+    this.classList.toggle('sidebar-wrapper')
+
+    $('.sidebar-nav li a').on ('click', function () {
+      $('#mySidenav').click();
+    });
+  });
